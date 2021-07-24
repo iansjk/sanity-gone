759bab71bcdf2348648803ae4bb1749feb267a30
Ian Kim <iansjk@gmail.com>
2021-07-24 19:54:21 -0400

    Add utility methods for images

---
+++ src/utils/globals.ts
@@ -0,0 +1,22 @@
+export function toTitleCase(string: string): string {
+  return [...string.toLowerCase()]
+    .map((char, i) => (i === 0 ? char.toUpperCase() : char))
+    .join("");
+}
+
+export function professionToClass(profession: string): string {
+  switch (profession) {
+    case "PIONEER":
+      return "Vanguard";
+    case "WARRIOR":
+      return "Guard";
+    case "SPECIAL":
+      return "Specialist";
+    case "TANK":
+      return "Defender";
+    case "SUPPORT":
+      return "Supporter";
+    default:
+      return toTitleCase(profession);
+  }
+}

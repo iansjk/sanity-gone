9757c5eda809e5f2cec533d8300c2f6e9d372153
Ian Kim <iansjk@gmail.com>
2021-07-25 16:22:53 -0400

    Implement grid spec for stat cells

--- src/components/OperatorStats.tsx
+++ src/components/OperatorStats.tsx
@@ -40,57 +40,88 @@ const OperatorStats: React.VFC<OperatorStatsProps> = (props) => {
   } = props;
   return (
     <dl css={styles}>
-      <dt>Damage Type</dt>
-      <dd>{damageType}</dd>
+      <div className="stat-cell">
+        <dt>Damage Type</dt>
+        <dd>{damageType}</dd>
+      </div>
 
-      <dt>Position</dt>
-      <dd>{position}</dd>
+      <div className="stat-cell">
+        <dt>Position</dt>
+        <dd>{position}</dd>
+      </div>
 
-      <dt>
-        <HealthIcon aria-hidden="true" /> Health
-      </dt>
-      <dd>{health}</dd>
+      <div className="stat-cell">
+        <dt>
+          <HealthIcon aria-hidden="true" /> Health
+        </dt>
+        <dd>{health}</dd>
+      </div>
 
-      <dt>
-        <AttackPowerIcon aria-hidden="true" /> Attack Power
-      </dt>
-      <dd>{attackPower}</dd>
+      <div className="stat-cell">
+        <dt>
+          <AttackPowerIcon aria-hidden="true" /> Attack Power
+        </dt>
+        <dd>{attackPower}</dd>
+      </div>
 
-      <dt>
-        <DefenseIcon aria-hidden="true" /> Defense
-      </dt>
-      <dd>{defense}</dd>
+      <div className="stat-cell">
+        <dt>
+          <DefenseIcon aria-hidden="true" /> Defense
+        </dt>
+        <dd>{defense}</dd>
+      </div>
 
-      <dt>
-        <AttackSpeedIcon aria-hidden="true" /> Attack Interval
-      </dt>
-      <dd>{attacksPerSecond} sec</dd>
+      <div className="stat-cell">
+        <dt>
+          <AttackSpeedIcon aria-hidden="true" /> Attack Interval
+        </dt>
+        <dd>{attacksPerSecond} sec</dd>
+      </div>
 
-      <dt>
-        <ArtsResistanceIcon aria-hidden="true" /> Arts Resistance
-      </dt>
-      <dd>{artsResistance}%</dd>
+      <div className="stat-cell">
+        <dt>
+          <ArtsResistanceIcon aria-hidden="true" /> Arts Resistance
+        </dt>
+        <dd>{artsResistance}%</dd>
+      </div>
 
-      <dt>
-        <BlockIcon aria-hidden="true" /> Block
-      </dt>
-      <dd>{blockCount}</dd>
+      <div className="stat-cell">
+        <dt>
+          <BlockIcon aria-hidden="true" /> Block
+        </dt>
+        <dd>{blockCount}</dd>
+      </div>
 
-      <dt>
-        <RedeployTimeIcon aria-hidden="true" /> Redeploy Time
-      </dt>
-      <dd>{redeployTimeInSeconds} sec</dd>
+      <div className="stat-cell">
+        <dt>
+          <RedeployTimeIcon aria-hidden="true" /> Redeploy Time
+        </dt>
+        <dd>{redeployTimeInSeconds} sec</dd>
+      </div>
 
-      <dt>
-        <DPCostIcon aria-hidden="true" /> DP Cost
-      </dt>
-      <dd>{dpCost}</dd>
+      <div className="stat-cell">
+        <dt>
+          <DPCostIcon aria-hidden="true" /> DP Cost
+        </dt>
+        <dd>{dpCost}</dd>
+      </div>
 
-      <dt>Range</dt>
-      <dd>{range}</dd>
+      <div className="stat-cell range">
+        <dt>Range</dt>
+        <dd>{range}</dd>
+      </div>
     </dl>
   );
 };
 export default OperatorStats;
 
-const styles = css``;
+const styles = css`
+  display: grid;
+  grid-template-rows: repeat(2, 1fr);
+  grid-template-columns: 180fr repeat(4, 140fr) 240fr;
+  grid-auto-flow: column;
+
+  .range {
+    grid-row-start: span 2;
+  }
+`;

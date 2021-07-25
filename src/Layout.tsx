f5beefb509ce234af9dfb2bc692417fb9c12c449
Ian Kim <me@ian.kim>
2021-07-25 18:31:36 -0400

    Add typing for theme

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,11 +1,13 @@
 /** @jsxImportSource @emotion/react */
 
+import { Theme } from "@emotion/react";
+
 const Layout: React.FC = ({ children }) => {
   return <main css={styles}>{children}</main>;
 };
 export default Layout;
 
-const styles = (theme) => ({
+const styles = (theme: Theme) => ({
   fontFamily: theme.typography.body.family,
   fontSize: theme.typography.body.size,
   color: theme.palette.white,
---
+++ src/emotion.d.ts
@@ -0,0 +1,16 @@
+import '@emotion/react'
+
+type TypographyProps = Partial<{
+  size: number;
+  family: string;
+}>;
+
+declare module '@emotion/react' {
+  export interface Theme {
+    palette: Record<string, string>,
+    typography: {
+      body: TypographyProps,
+    },
+    spacing: (multiple: number) => number
+  }
+}

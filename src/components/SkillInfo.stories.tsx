e8cb6280c51f73a9d2adc5374d298ccfe6ad0886
Ian Kim <iansjk@gmail.com>
2021-07-26 01:20:07 -0400

    Define prop types for SkillInfo

--- src/components/SkillInfo.stories.tsx
+++ src/components/SkillInfo.stories.tsx
@@ -1,5 +1,5 @@
 import { Story, Meta } from "@storybook/react";
-import SkillInfo, { SkillInfoProps } from "./SkillInfo";
+import SkillInfo, { SkillInfoProps, SkillLevelObject } from "./SkillInfo";
 
 export default {
   title: "Skills/SkillInfo",
@@ -9,4 +9,116 @@ export default {
 const Template: Story<SkillInfoProps> = (args) => <SkillInfo {...args} />;
 
 export const Default = Template.bind({});
-Default.args = {};
+Default.args = {
+  skillObject: {
+    skillId: "skchr_mudrok_3",
+    iconId: null,
+    hidden: false,
+    levels: [
+      {
+        name: "Bloodline of Desecrated Earth",
+        range: {
+          id: "x-1",
+          direction: 1,
+          grids: [
+            {
+              row: 2,
+              col: 0,
+            },
+            {
+              row: 1,
+              col: -1,
+            },
+            {
+              row: 1,
+              col: 0,
+            },
+            {
+              row: 1,
+              col: 1,
+            },
+            {
+              row: 0,
+              col: -2,
+            },
+            {
+              row: 0,
+              col: -1,
+            },
+            {
+              row: 0,
+              col: 0,
+            },
+            {
+              row: 0,
+              col: 1,
+            },
+            {
+              row: 0,
+              col: 2,
+            },
+            {
+              row: -1,
+              col: -1,
+            },
+            {
+              row: -1,
+              col: 0,
+            },
+            {
+              row: -1,
+              col: 1,
+            },
+            {
+              row: -2,
+              col: 0,
+            },
+          ],
+        },
+        description:
+          "Upon skill activation, Mudrock stops attacking and does not take damage for <@ba.vup>{sleep}</> seconds, reducing the movement speed of surrounding enemies by <@ba.vup>-{-move_speed:0%}</>; After this state ends, Mudrock stuns surrounding ground enemies for <@ba.vup>{stun}</> seconds and gains ATK <@ba.vup>+{atk:0%}</>, <@ba.vup>reduced</> attack interval, DEF <@ba.vup>+{def:0%}</>, and the ability to attack all blocked enemies",
+        skillType: 1,
+        spData: {
+          spType: 1,
+          levelUpCost: null,
+          maxChargeTime: 1,
+          spCost: 25,
+          initSp: 15,
+          increment: 1.0,
+        },
+        prefabId: "skchr_mudrok_3",
+        duration: 30.0,
+        blackboard: [
+          {
+            key: "atk",
+            value: 1.4,
+          },
+          {
+            key: "def",
+            value: 0.8,
+          },
+          {
+            key: "move_speed",
+            value: -0.6,
+          },
+          {
+            key: "sleep",
+            value: 10.0,
+          },
+          {
+            key: "awake",
+            value: 20.0,
+          },
+          {
+            key: "base_attack_time",
+            value: -0.3,
+          },
+          {
+            key: "stun",
+            value: 5.0,
+          },
+        ],
+      } as SkillLevelObject,
+    ],
+  },
+} as SkillInfoProps;
--- src/components/SkillInfo.tsx
+++ src/components/SkillInfo.tsx
@@ -1,6 +1,41 @@
-export interface SkillInfoProps {}
+import OperatorRange, { RangeObject } from "./OperatorRange";
 
-const SkillInfo: React.VFC<SkillInfoProps> = (props) => {
-  return <></>;
+export interface SkillLevelObject {
+  name: string;
+  description: string;
+  // SkillLevelObject only has rangeId (of type string) in the game data,
+  // but we expect it to be denormalized into a RangeObject before being passed to <SkillInfo />
+  range: RangeObject;
+  spData: {
+    spType: number;
+    spCost: number;
+    initSp: number;
+  };
+  duration: number;
+  // "blackboard" is used for interpolating formatted numeric values into the description,
+  // e.g. "gains ATK <@ba.vup>+{atk:0%}</>, <@ba.vup>reduced</> attack interval, DEF <@ba.vup>+{def:0%}</>, ..."
+  // references blackboard.atk, blackboard.def and is formatted to
+  // "gains ATK +140%, reduced attack interval, DEF +80%, ..." with blue text for the interpolated values
+  blackboard: {
+    key: string;
+    value: number;
+  }[];
+}
+
+export interface SkillInfoProps {
+  skillObject: {
+    skillId: string;
+    iconId: string | null;
+    levels: SkillLevelObject[];
+  };
+}
+
+const SkillInfo: React.VFC<SkillInfoProps> = ({ skillObject }) => {
+  const { skillId, iconId, levels } = skillObject;
+  const { name, description, spData, range, duration } = levels[
+    levels.length - 1
+  ];
+  const { initSp, spCost } = spData;
+  return <OperatorRange rangeObject={range} />;
 };
 export default SkillInfo;

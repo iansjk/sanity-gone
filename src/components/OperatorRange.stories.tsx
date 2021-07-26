e45733d7a220014855b4fe709bb4dfeb5c68350b
Ian Kim <me@ian.kim>
2021-07-25 20:16:43 -0400

    Implement basic OperatorRange

---
+++ src/components/OperatorRange.stories.tsx
@@ -0,0 +1,74 @@
+import { Meta, Story } from "@storybook/react";
+import OperatorRange, { OperatorRangeProps } from "./OperatorRange";
+
+export default {
+  title: "Introduction/OperatorRange",
+  component: OperatorRange,
+} as Meta;
+
+const Template: Story<OperatorRangeProps> = (args) => (
+  <OperatorRange {...args} />
+);
+export const Cuora = Template.bind({});
+Cuora.args = {
+  rangeObject: {
+    id: "0-1",
+    direction: 1,
+    grids: [
+      {
+        row: 0,
+        col: 0,
+      },
+    ],
+  },
+};
+export const Mudrock = Template.bind({});
+Mudrock.args = {
+  rangeObject: {
+    id: "1-1",
+    direction: 1,
+    grids: [
+      {
+        row: 0,
+        col: 0,
+      },
+      {
+        row: 0,
+        col: 1,
+      },
+    ],
+  },
+};
+export const IfritE2 = Template.bind({});
+IfritE2.args = {
+  rangeObject: {
+    id: "5-1",
+    direction: 1,
+    grids: [
+      {
+        row: 0,
+        col: 0,
+      },
+      {
+        row: 0,
+        col: 1,
+      },
+      {
+        row: 0,
+        col: 2,
+      },
+      {
+        row: 0,
+        col: 3,
+      },
+      {
+        row: 0,
+        col: 4,
+      },
+      {
+        row: 0,
+        col: 5,
+      },
+    ],
+  },
+};
--- src/components/OperatorRange.tsx
+++ src/components/OperatorRange.tsx
@@ -10,13 +10,19 @@ interface RangeObject {
   }[];
 }
 
-interface OperatorRangeProps {
-  rangeObject: RangeObject;
+enum GridCell {
+  Operator = "O",
+  empty = ".",
+  active = "x",
 }
 
-const tableSize = (
-  rangeObject: RangeObject
-): { rows: number; cols: number } => {
+interface NormalizedRange {
+  rows: number;
+  cols: number;
+  grid: GridCell[][];
+}
+
+const normalizeRange = (rangeObject: RangeObject): NormalizedRange => {
   // for each of rows and cols,
   // find the minimum value and the maximum value
   // then return max-min to get number of rows/cols
@@ -26,16 +32,37 @@ const tableSize = (
   const maxRowIndex = Math.max(...rowIndices);
   const minColIndex = Math.min(...colIndices);
   const maxColIndex = Math.max(...colIndices);
-  return { rows: maxRowIndex - minRowIndex, cols: maxColIndex - minColIndex };
+
+  // create a 2d-array of size [rows, cols]
+  const rows = maxRowIndex - minRowIndex + 1;
+  const cols = maxColIndex - minColIndex + 1;
+  const grid: GridCell[][] = Array(rows)
+    .fill(GridCell.empty)
+    .map(() => Array(cols).fill(GridCell.empty));
+  rangeObject.grids.forEach((cell) => {
+    const type =
+      cell.row === 0 && cell.col === 0 ? GridCell.Operator : GridCell.active;
+    grid[cell.row - minRowIndex][cell.col - minColIndex] = type;
+  });
+  return {
+    rows,
+    cols,
+    grid,
+  };
 };
 
+export interface OperatorRangeProps {
+  rangeObject: RangeObject;
+}
+
 const OperatorRange: React.VFC<OperatorRangeProps> = ({ rangeObject }) => {
-  const { rows, cols } = tableSize(rangeObject);
+  const { rows, cols, grid } = normalizeRange(rangeObject);
 
   return (
     <table>
       <thead>
         <tr>
+          <th></th>
           {[...Array(cols).keys()].map((i) => (
             <th key={i} scope="col">{`Y${i + 1}`}</th>
           ))}
@@ -46,7 +73,7 @@ const OperatorRange: React.VFC<OperatorRangeProps> = ({ rangeObject }) => {
           <tr key={rowIndex}>
             <th scope="row">{`X${rowIndex + 1}`}</th>
             {[...Array(cols).keys()].map((colIndex) => (
-              <td key={colIndex}></td>
+              <td key={colIndex}>{grid[rowIndex][colIndex]}</td>
             ))}
           </tr>
         ))}

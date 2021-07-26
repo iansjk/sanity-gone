789257b121b32673704a2833a98538138bb491ed
Ian Kim <iansjk@gmail.com>
2021-07-26 00:27:20 -0400

    Stub Skills section component

---
+++ src/components/Skills.tsx
@@ -0,0 +1,36 @@
+/** @jsxImportSource @emotion/react */
+import { Theme, css } from "@emotion/react";
+import CardWithPanels from "./CardWithPanels";
+import CardPanel from "./CardPanel";
+
+export type SkillsProps = Partial<{
+  skill1Analysis: string;
+  skill2Analysis: string;
+  skill3Analysis: string;
+}>;
+
+const Skills: React.VFC<SkillsProps> = (props) => {
+  const { skill1Analysis, skill2Analysis, skill3Analysis } = props;
+
+  return (
+    <CardWithPanels header="Skills" buttonPrefix="S" css={styles}>
+      {skill1Analysis && (
+        <CardPanel dangerouslySetInnerHTML={{ __html: skill1Analysis }} />
+      )}
+      {skill2Analysis && (
+        <CardPanel dangerouslySetInnerHTML={{ __html: skill2Analysis }} />
+      )}
+      {skill3Analysis && (
+        <CardPanel dangerouslySetInnerHTML={{ __html: skill3Analysis }} />
+      )}
+    </CardWithPanels>
+  );
+};
+export default Skills;
+
+const styles = (theme: Theme) => css`
+  b,
+  strong {
+    color: ${theme.palette.pink};
+  }
+`;

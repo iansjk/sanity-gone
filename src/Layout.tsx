763d0b5503c2146b833e28390dd926299d09636b
Ian Kim <iansjk@gmail.com>
2021-07-26 01:36:51 -0400

    Use style string instead of object

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -16,28 +16,32 @@ const Layout: React.FC = ({ children }) => {
 };
 export default Layout;
 
-const styles = (theme: Theme) => ({
-  fontFamily: theme.typography.body.family,
-  fontSize: theme.typography.body.size,
-  color: theme.palette.white,
-  backgroundColor: theme.palette.background,
-  padding: theme.spacing(2),
-  "& b, & strong": {
-    color: theme.palette.blue,
-    fontWeight: theme.typography.highlight.weight,
-  },
-  "& a": {
-    fontStyle: theme.typography.link.fontStyle,
-    textDecoration: theme.typography.link.textDecoration,
-    color: theme.palette.blue,
-  },
-  "& .visually-hidden:not(:focus):not(:active)": {
-    clip: "rect(0 0 0 0)",
-    clipPath: "inset(50%)",
-    height: "1px",
-    overflow: "hidden",
-    position: "absolute" as const,
-    whiteSpace: "nowrap" as const,
-    width: "1px",
-  },
-});
+const styles = (theme: Theme) => css`
+  font-family: ${theme.typography.body.family};
+  font-size: ${theme.typography.body.size};
+  color: ${theme.palette.white};
+  background-color: ${theme.palette.background};
+  padding: ${theme.spacing(2)};
+
+  b,
+  strong {
+    color: ${theme.palette.blue};
+    font-weight: ${theme.typography.highlight.weight};
+  }
+
+  a {
+    font-style: ${theme.typography.link.fontStyle};
+    text-decoration: ${theme.typography.link.textDecoration};
+    color: ${theme.palette.blue};
+  }
+
+  .visually-hidden:not(:focus):not(:active) {
+    clip: rect(0 0 0 0);
+    clip-path: inset(50%);
+    height: 1px;
+    overflow: hidden;
+    position: absolute;
+    white-space: nowrap;
+    width: 1px;
+  }
+`;

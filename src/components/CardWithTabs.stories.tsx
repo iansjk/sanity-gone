6c939b866cdc73aea7a61bfd68b9d16c786f4ddc
Ian Kim <iansjk@gmail.com>
2021-07-27 17:43:04 -0400

    Wrap doggo ipsum text in <p>

--- src/components/CardWithTabs.stories.tsx
+++ src/components/CardWithTabs.stories.tsx
@@ -12,24 +12,30 @@ export default {
 export const Default: Story<CardWithTabsProps> = (args) => (
   <CardWithTabs {...args}>
     <CardPanel>
-      Doggo ipsum such treat doggo wow such tempt noodle horse, boofers. waggy
-      wags. Shooberino heckin good boys borkf very jealous pupper, noodle horse
-      heckin good boys. Bork very hand that feed shibe borkf, doge. Long water
-      shoob doggo porgo doge borkf, wow very biscit fluffer. Wow very biscit
-      fluffer shibe sub woofer, fat boi wow such tempt.
+      <p>
+        Doggo ipsum such treat doggo wow such tempt noodle horse, boofers. waggy
+        wags. Shooberino heckin good boys borkf very jealous pupper, noodle
+        horse heckin good boys. Bork very hand that feed shibe borkf, doge. Long
+        water shoob doggo porgo doge borkf, wow very biscit fluffer. Wow very
+        biscit fluffer shibe sub woofer, fat boi wow such tempt.
+      </p>
     </CardPanel>
     <CardPanel>
-      Very jealous pupper heck shoob woofer long water shoob heckin angery
-      woofer long woofer, lotsa pats extremely cuuuuuute long doggo he made many
-      woofs. Heckin angery woofer doing me a frighten smol, doggorino. puggorino
-      much ruin diet. He made many woofs waggy wags long bois smol borking doggo
-      with a long snoot for pats, smol.
+      <p>
+        Very jealous pupper heck shoob woofer long water shoob heckin angery
+        woofer long woofer, lotsa pats extremely cuuuuuute long doggo he made
+        many woofs. Heckin angery woofer doing me a frighten smol, doggorino.
+        puggorino much ruin diet. He made many woofs waggy wags long bois smol
+        borking doggo with a long snoot for pats, smol.
+      </p>
     </CardPanel>
     <CardPanel>
-      Borking doggo aqua doggo porgo big ol vvv, aqua doggo doge long doggo.
-      Shibe snoot boof porgo sub woofer, blop heck very taste wow maximum
-      borkdrive super chub, he made many woofs blop smol. vvv pupperino wow very
-      biscit. Heck wow very biscit what a nice floof, much ruin diet.
+      <p>
+        Borking doggo aqua doggo porgo big ol vvv, aqua doggo doge long doggo.
+        Shibe snoot boof porgo sub woofer, blop heck very taste wow maximum
+        borkdrive super chub, he made many woofs blop smol. vvv pupperino wow
+        very biscit. Heck wow very biscit what a nice floof, much ruin diet.
+      </p>
     </CardPanel>
   </CardWithTabs>
 );

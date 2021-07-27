2b61e580c4869e2aee8833a9eafd74c1f4d142b8
Ian Kim <iansjk@gmail.com>
2021-07-27 12:52:04 -0400

    Update skill info icons as well

--- src/components/icons/skillInfo/InitialSPIcon.tsx
+++ src/components/icons/skillInfo/InitialSPIcon.tsx
@@ -1,14 +1,14 @@
 const InitialSPIcon: React.VFC<React.HTMLAttributes<SVGElement>> = (props) => {
   return (
     <svg
-      width="9"
-      height="10"
-      viewBox="0 0 9 10"
+      width="14"
+      height="14"
+      viewBox="0 0 14 14"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
       {...props}
     >
-      <path d="M0.851562 10V0L8.7826 5L0.851562 10Z" fill="#C4C4C4" />
+      <path d="M2 13V1L12 7L2 13Z" fill="#D6D6E2" />
     </svg>
   );
 };
--- src/components/icons/skillInfo/SPCostIcon.tsx
+++ src/components/icons/skillInfo/SPCostIcon.tsx
@@ -1,14 +1,14 @@
 const SPCostIcon: React.VFC<React.HTMLAttributes<SVGElement>> = (props) => {
   return (
     <svg
-      width="8"
-      height="10"
-      viewBox="0 0 8 10"
+      width="14"
+      height="14"
+      viewBox="0 0 14 14"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
       {...props}
     >
-      <path d="M0 6L5 0L4.2 4H8L3 10L3.8 6H0Z" fill="#A7E855" />
+      <path d="M1 8.4L8.5 0L7.3 5.6H13L5.5 14L6.7 8.4H1Z" fill="#D6D6E2" />
     </svg>
   );
 };

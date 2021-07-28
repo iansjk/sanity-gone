2188057c7d5c97b3e53330103088429e49d02bbf
Ian Kim <iansjk@gmail.com>
2021-07-28 15:07:49 -0400

    Add some initial styling

--- src/components/SynergyOperator.stories.tsx
+++ src/components/SynergyOperator.stories.tsx
@@ -17,7 +17,7 @@ export const Default = Template.bind({});
 Default.args = {
   synergyOperator: {
     name: "Angelina",
-    quality: SynergyQuality.Good,
+    quality: SynergyQuality["Good Synergy"],
     analysis: `
       <p>Her benefits to Mudrock are entirely passive, and Angelina is great for general use anyway so it makes it super easy to have them together. Angelina passively provides global HP regen and an ASPD buff, since the regen is obviously not a direct heal, it works perfectly fine on Mudrock. The ASPD is also always good since Mudrock is also a damage dealer. The passive ASPD also helps if you’re bringing Blemishine too, since faster attacks means faster charging for Mudrock S2 while Blemi is deployed. Team building matters, folks! As a bonus, Angelina’s crowd control capabilities can help with stalling during Mudrock’s S3 startup, since her fast skill rotations means Angelina can always be ready to be used together with Mudrock’s S3.</p>
     `,
--- src/components/SynergyOperator.tsx
+++ src/components/SynergyOperator.tsx
@@ -1,8 +1,11 @@
+import { css, Theme } from "@emotion/react";
+import React from "react";
+
 export enum SynergyQuality {
   "Anti-Synergy" = -1,
-  "Decent",
-  "Good",
-  "Excellent",
+  "Decent Synergy",
+  "Good Synergy",
+  "Excellent Synergy",
 }
 
 export interface SynergyOperatorProps {
@@ -13,16 +16,50 @@ export interface SynergyOperatorProps {
   };
 }
 
-const SynergyOperator: React.VFC<SynergyOperatorProps> = (props) => {
-  const { name, quality, analysis } = props.synergyOperator;
+const SynergyOperator: React.VFC<
+  SynergyOperatorProps & React.HTMLAttributes<HTMLDivElement>
+> = (props) => {
+  const { name, quality, analysis, ...rest } = props.synergyOperator;
   return (
-    <>
-      <h3 className="synergy-operator-name">{name}</h3>
-      <span className={`synergy-quality quality-${quality}`}>
-        {SynergyQuality[quality]}
-      </span>
+    <section css={styles} {...rest}>
+      <div className="name-and-quality">
+        <h3 className="synergy-operator-name">{name}</h3>
+        <span className={`synergy-quality quality-${quality}`}>
+          {SynergyQuality[quality]}
+        </span>
+      </div>
       <div dangerouslySetInnerHTML={{ __html: analysis }} />
-    </>
+    </section>
   );
 };
 export default SynergyOperator;
+
+const styles = (theme: Theme) => css`
+  display: flex;
+  flex-direction: column;
+
+  .name-and-quality {
+    .synergy-operator-name {
+      font-size: 24px;
+      margin: 0 0 ${theme.spacing(1)};
+      font-weight: ${theme.typography.highlight.weight};
+    }
+
+    .synergy-quality {
+      text-transform: uppercase;
+      color: ${theme.palette.gray};
+
+      &.quality--1 {
+        color: ${theme.palette.red};
+      }
+
+      &.quality-1 {
+        color: ${theme.palette.blue};
+      }
+
+      &.quality-2 {
+        color: ${theme.palette.lime};
+      }
+    }
+  }
+`;

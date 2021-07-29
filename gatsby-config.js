10e5288ea1e51cd82323293714ce01d11a84d04c
Ian Kim <me@ian.kim>
2021-07-29 17:37:37 -0400

    Add Contentful integration

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,18 @@
+require("dotenv").config();
+
 module.exports = {
   siteMetadata: {
     siteUrl: "https://www.yourdomain.tld",
     title: "Sanity;Gone 0",
   },
-  plugins: ["gatsby-plugin-emotion"],
+  plugins: [
+    "gatsby-plugin-emotion",
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: `9auzhr5vyq9m`,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+      },
+    },
+  ],
 };

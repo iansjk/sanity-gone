43b2c5fc1819b354e325400be035f7c6aa3941ca
Ian Kim <iansjk@gmail.com>
2021-07-29 21:40:57 -0400

    Add graphql query for Operator Analysis content model

--- src/pages/operators/{contentfulOperatorAnalysis.operator__name}.tsx
+++ src/pages/operators/{contentfulOperatorAnalysis.operator__name}.tsx
@@ -1,3 +1,5 @@
+import { graphql, useStaticQuery } from "gatsby";
+import { SynergyQuality } from "../../components/SynergyOperator";
 import Layout from "../../Layout";
 
 interface Props {
@@ -5,12 +7,102 @@ interface Props {
   params: any;
 }
 
+interface MarkdownNode {
+  childMarkdownRemark: {
+    html: string;
+  };
+}
+
+interface OperatorAnalysisData {
+  operator: {
+    accentColorInHex: string;
+    archetype: string;
+    limited: boolean;
+    name: string;
+    operatorImageUrl: string;
+  };
+  author: {
+    authorDiscordTag: string;
+  }[];
+  introduction: MarkdownNode;
+  talent1Analysis: MarkdownNode;
+  talent2Analysis: MarkdownNode;
+  skill1Analysis: MarkdownNode;
+  skill2Analysis: MarkdownNode;
+  skill3Analysis: MarkdownNode;
+  operatorSynergies: {
+    operatorName: string;
+    synergyQuality: SynergyQuality;
+    synergyDescription: MarkdownNode;
+  };
+  summary: MarkdownNode;
+}
+
 const OperatorAnalysis: React.VFC<Props> = (props) => {
   const { params, pageContext } = props;
+  const data: OperatorAnalysisData = useStaticQuery(graphql`
+    query($id: String) {
+      contentfulOperatorAnalysis(id: { eq: $id }) {
+        operator {
+          accentColorInHex
+          archetype
+          limited
+          name
+          operatorImageUrl
+        }
+        author {
+          authorDiscordTag
+        }
+        introduction {
+          childMarkdownRemark {
+            html
+          }
+        }
+        talent1Analysis {
+          childMarkdownRemark {
+            html
+          }
+        }
+        talent2Analysis {
+          childMarkdownRemark {
+            html
+          }
+        }
+        skill1Analysis {
+          childMarkdownRemark {
+            html
+          }
+        }
+        skill2Analysis {
+          childMarkdownRemark {
+            html
+          }
+        }
+        skill3Analysis {
+          childMarkdownRemark {
+            html
+          }
+        }
+        operatorSynergies {
+          operatorName
+          synergyQuality
+          synergyDescription {
+            childMarkdownRemark {
+              html
+            }
+          }
+        }
+        summary {
+          childMarkdownRemark {
+            html
+          }
+        }
+      }
+    }
+  `);
   return (
     <Layout>
-      <pre>params: {JSON.stringify(params, null, 2)}</pre>
-      <pre>pageContext: {JSON.stringify(pageContext, null, 2)}</pre>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
     </Layout>
   );
 };

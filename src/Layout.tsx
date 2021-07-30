14f7ad41541055963840f6f299e4012051c60cee
Ian Kim <iansjk@gmail.com>
2021-07-30 18:40:54 -0400

    Add actual variety of font weights
    Previously we were only loading 400

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -10,10 +10,15 @@ const Layout: React.FC = ({ children }) => {
     <Fragment>
       <Helmet>
         <title>Sanity;Gone 0</title>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
-          href="https://fonts.googleapis.com/css?family=Source+Sans+Pro"
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin=""
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:ital,wght@0,400;0,600;0,700;1,400;1,600;1,700&display=swap"
           rel="stylesheet"
-          type="text/css"
         />
       </Helmet>
       <ThemeProvider theme={defaultTheme}>

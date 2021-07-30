9e2c1720e292b9b2fc3649f4242c73f16eb9a49f
Ian Kim <iansjk@gmail.com>
2021-07-29 22:55:12 -0400

    Pull in (almost) all data as a page query
    Still missing skills at the moment

--- src/pages/operators/{contentfulOperatorAnalysis.operator__name}.tsx
+++ src/pages/operators/{contentfulOperatorAnalysis.operator__name}.tsx
@@ -1,4 +1,4 @@
-import { graphql, useStaticQuery } from "gatsby";
+import { graphql } from "gatsby";
 import { SynergyQuality } from "../../components/SynergyOperator";
 import Layout from "../../Layout";
 
@@ -39,71 +39,126 @@ interface OperatorAnalysisData {
 }
 
 const OperatorAnalysis: React.VFC<Props> = (props) => {
-  const { params, pageContext } = props;
-  const data: OperatorAnalysisData = useStaticQuery(graphql`
-    query($id: String) {
-      contentfulOperatorAnalysis(id: { eq: $id }) {
-        operator {
-          accentColorInHex
-          archetype
-          limited
-          name
-          operatorImageUrl
+  const { params, pageContext, ...rest } = props;
+
+  return (
+    <Layout>
+      <pre>params: {JSON.stringify(params, null, 2)}</pre>
+      <pre>pageContext: {JSON.stringify(pageContext, null, 2)}</pre>
+      <pre>{JSON.stringify(rest, null, 2)}</pre>
+    </Layout>
+  );
+};
+export default OperatorAnalysis;
+
+export const query = graphql`
+  query($id: String!, $operator__name: String!) {
+    contentfulOperatorAnalysis(id: { eq: $id }) {
+      operator {
+        accentColorInHex
+        archetype
+        limited
+        name
+        operatorImageUrl
+      }
+      author {
+        authorDiscordTag
+      }
+      introduction {
+        childMarkdownRemark {
+          html
         }
-        author {
-          authorDiscordTag
+      }
+      talent1Analysis {
+        childMarkdownRemark {
+          html
         }
-        introduction {
-          childMarkdownRemark {
-            html
-          }
+      }
+      talent2Analysis {
+        childMarkdownRemark {
+          html
         }
-        talent1Analysis {
-          childMarkdownRemark {
-            html
-          }
+      }
+      skill1Analysis {
+        childMarkdownRemark {
+          html
         }
-        talent2Analysis {
-          childMarkdownRemark {
-            html
-          }
+      }
+      skill2Analysis {
+        childMarkdownRemark {
+          html
         }
-        skill1Analysis {
-          childMarkdownRemark {
-            html
-          }
+      }
+      skill3Analysis {
+        childMarkdownRemark {
+          html
         }
-        skill2Analysis {
-          childMarkdownRemark {
-            html
-          }
-        }
-        skill3Analysis {
-          childMarkdownRemark {
-            html
-          }
-        }
-        operatorSynergies {
-          operatorName
-          synergyQuality
-          synergyDescription {
-            childMarkdownRemark {
-              html
-            }
-          }
-        }
-        summary {
+      }
+      operatorSynergies {
+        operatorName
+        synergyQuality
+        synergyDescription {
           childMarkdownRemark {
             html
           }
         }
       }
+      summary {
+        childMarkdownRemark {
+          html
+        }
+      }
     }
-  `);
-  return (
-    <Layout>
-      <pre>{JSON.stringify(data, null, 2)}</pre>
-    </Layout>
-  );
-};
-export default OperatorAnalysis;
+
+    operatorsJson(name: { eq: $operator__name }) {
+      name
+      profession
+      position
+      description
+      rarity
+      talents {
+        candidates {
+          unlockCondition {
+            phase
+            level
+          }
+          requiredPotentialRank
+          name
+          description
+          range {
+            grids {
+              row
+              col
+            }
+          }
+          blackboard {
+            key
+            value
+          }
+        }
+      }
+      phases {
+        range {
+          grids {
+            row
+            col
+          }
+        }
+        maxLevel
+        attributesKeyFrames {
+          level
+          data {
+            maxHp
+            atk
+            def
+            baseAttackTime
+            magicResistance
+            cost
+            blockCnt
+            respawnTime
+          }
+        }
+      }
+    }
+  }
+`;

802d1cc0c0dd31ed205ecb04a18c25053db437d4
Ian Kim <iansjk@gmail.com>
2021-07-31 16:47:44 -0400

    Fix Storybook babel config

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,25 +1,21 @@
 // The location of your Babel config.
-const babelConfig = require('../babel.config.js');
+const babelConfig = require("../babel.config.js");
 
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials"
-  ],
-  babel: async (
-    options
-  ) => {
-    options
-      .overrides
-      .push({
-        ...babelConfig,
-        test: '*', // This says "for all files, use this override".
-      })
+  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+  babel: async (options) => {
+    options.overrides.push({
+      presets: [
+        [
+          "@babel/preset-react",
+          { runtime: "automatic", importSource: "@emotion/react" },
+        ],
+      ],
+      plugins: ["@emotion/babel-plugin"],
+      test: "*", // This says "for all files, use this override".
+    });
 
     return options;
   },
-}
+};

8504f409988bad79d2d487228c5eb9c0c5a17773
Ian Kim <iansjk@gmail.com>
2021-08-01 17:08:29 -0400

    Add theme containerWidth, contentY to types

--- src/emotion.d.ts
+++ src/emotion.d.ts
@@ -7,5 +7,7 @@ declare module "@emotion/react" {
     palette: typeof defaultTheme.palette;
     typography: typeof defaultTheme.typography;
     spacing: typeof defaultTheme.spacing;
+    containerWidth: string;
+    contentY: string;
   }
 }

d007289a2f651635552933969fddb2235c9298fe
Ian Kim <iansjk@gmail.com>
2021-08-03 09:44:08 -0400

    Render gallery images and captions

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -1,9 +1,45 @@
+import parse, { attributesToProps } from "html-react-parser";
+import { Element } from "domhandler/lib/node";
+import { css, Theme } from "@emotion/react";
+
 export interface GalleryProps {
   contents: string; // htmlstring
 }
 
 const Gallery: React.FC<GalleryProps> = (props) => {
   const { contents } = props;
-  return <div>{contents}</div>;
+  return (
+    <div css={styles}>
+      {parse(contents, {
+        replace: (domNode) => {
+          if (domNode instanceof Element && domNode.name === "img") {
+            return (
+              <div className="gallery-image">
+                <img {...attributesToProps(domNode.attribs)} />
+                <span className="image-caption" aria-hidden="true">
+                  {domNode.attribs.alt}
+                </span>
+              </div>
+            );
+          }
+        },
+      })}
+    </div>
+  );
 };
 export default Gallery;
+
+const styles = (theme: Theme) => css`
+  display: flex;
+  justify-content: space-between;
+
+  .gallery-image {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    .image-caption {
+      display: inline-block;
+    }
+  }
+`;

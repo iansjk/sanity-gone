7a865075a55000c69c35e3929159e4b66f6921f5
Ian Kim <iansjk@gmail.com>
2021-10-16 15:43:12 -0400

    Change card header bg to midtone-1 66%

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -31,7 +31,7 @@ const styles = (theme: Theme) => css`
 
   .heading-block {
     padding: ${theme.spacing(2, 0, 2, 4)};
-    background-color: ${transparentize(0.34, theme.palette.dark)};
+    background-color: ${transparentize(0.34, theme.palette.midtoneDarker)};
     border-top-right-radius: ${theme.spacing(1)};
 
     ${theme.breakpoints.down("mobile")} {

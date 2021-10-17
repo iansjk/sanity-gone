c18394b19d7a56f5b8ae78b0462ddf9cacdd1b55
Ian Kim <iansjk@gmail.com>
2021-10-17 00:16:04 -0400

    Add some initial text styling to operator items

--- src/pages/operators/index.tsx
+++ src/pages/operators/index.tsx
@@ -2,6 +2,7 @@ import { graphql } from "gatsby";
 import { css, Theme } from "@emotion/react";
 import Layout from "../../Layout";
 import { operatorPortrait } from "../../utils/images";
+import { professionToClass } from "../../utils/globals";
 
 interface Props {
   data: {
@@ -31,7 +32,16 @@ const Operators: React.VFC<Props> = (props) => {
               className="operator"
               //@ts-expect-error css variable
               style={{ "--bg-image": `url("${operatorPortrait(op.name)}")` }}
-            ></li>
+            >
+              <div className="operator-info">
+                <span className="operator-name">{op.name}</span>
+                <span className="rarity">{op.rarity + 1}</span>
+                <span className="operator-class">
+                  {professionToClass(op.profession)}
+                </span>
+              </div>
+              <span className="operator-subclass"></span>
+            </li>
           ))}
         </ul>
       </main>
@@ -53,6 +63,8 @@ const styles = (theme: Theme) => css`
     li.operator {
       width: 160px;
       height: 280px;
+      display: grid;
+      grid-template-rows: max-content 1fr max-content;
       border-radius: ${theme.spacing(0.5)};
       background-size: cover;
       background-image: linear-gradient(
@@ -65,6 +77,32 @@ const styles = (theme: Theme) => css`
       /* &:hover {
         border-bottom: ${theme.spacing(0.5)} solid ${theme.palette.gray};
       } */
+
+      .operator-info {
+        grid-row: 3;
+        display: grid;
+        grid-template-rows: repeat(2, max-content);
+        grid-template-columns: 1fr max-content;
+        padding: ${theme.spacing(2)};
+        row-gap: ${theme.spacing(1)};
+
+        .operator-name {
+          grid-column: span 2;
+        }
+
+        .rarity {
+          grid-column: 2;
+        }
+
+        .operator-class {
+          grid-row: 2;
+          text-transform: uppercase;
+        }
+      }
+
+      .operator-subclass {
+        grid-row: 1;
+      }
     }
   }
 `;

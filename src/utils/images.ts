6c2d54877e86bbe840a342acb1d154be9df41064
Ian Kim <iansjk@gmail.com>
2021-10-18 17:35:58 -0400

    Don't suffix Cloudinary base url with arknights/

--- src/utils/images.ts
+++ src/utils/images.ts
@@ -1,33 +1,33 @@
 import { slugify } from "./globals";
 
 const CLOUDINARY_BASE_URL =
-  "https://res.cloudinary.com/samidare/image/upload/v1/arknights";
+  "https://res.cloudinary.com/samidare/image/upload/v1/";
 
 export const operatorImage = (name: string, elite?: number): string =>
-  `${CLOUDINARY_BASE_URL}/operators/${slugify(name)}${
+  `${CLOUDINARY_BASE_URL}/arknights/operators/${slugify(name)}${
     typeof elite !== "undefined" && (elite > 1 || name === "Amiya")
       ? `-elite-${elite}`
       : ""
   }`;
 
 export const summonImage = (id: string): string =>
-  `${CLOUDINARY_BASE_URL}/summons/${id}`;
+  `${CLOUDINARY_BASE_URL}/arknights/summons/${id}`;
 
 export const operatorPortrait = (name: string, elite?: number): string =>
-  `${CLOUDINARY_BASE_URL}/portraits/${slugify(name)}${
+  `${CLOUDINARY_BASE_URL}/arknights/portraits/${slugify(name)}${
     typeof elite !== "undefined" && (elite > 1 || name === "Amiya")
       ? `-elite-${elite}`
       : ""
   }`;
 
 export const operatorClassIcon = (operatorClass: string): string =>
-  `${CLOUDINARY_BASE_URL}/classes/${operatorClass}`;
+  `${CLOUDINARY_BASE_URL}/arknights/classes/${operatorClass}`;
 
 export const operatorSubclassIcon = (subProfessionId: string): string =>
-  `${CLOUDINARY_BASE_URL}/subclasses/${subProfessionId}`;
+  `${CLOUDINARY_BASE_URL}/arknights/subclasses/${subProfessionId}`;
 
 export const skillIcon = (iconId: string | null, skillId: string): string =>
-  `${CLOUDINARY_BASE_URL}/skills/${iconId ?? skillId}`;
+  `${CLOUDINARY_BASE_URL}/arknights/skills/${iconId ?? skillId}`;
 
 export const moduleImage = (moduleId: string): string =>
-  `${CLOUDINARY_BASE_URL}/equip/${moduleId}`;
+  `${CLOUDINARY_BASE_URL}/arknights/equip/${moduleId}`;

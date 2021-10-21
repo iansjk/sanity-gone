81e6cfdd4f06b557d6fdf9b4c757b5e7f992046b
Ian Kim <iansjk@gmail.com>
2021-10-21 09:20:38 -0400

    Implement initial version of MasteryRecommendation

---
+++ src/components/MasteryRecommendation.tsx
@@ -0,0 +1,57 @@
+import { Theme, css } from "@emotion/react";
+
+export interface MasteryRecommendationProps {
+  level: "no" | "1" | "2" | "3";
+  priority?: string;
+  analysis: string;
+}
+
+const MasteryRecommendation: React.VFC<MasteryRecommendationProps> = (
+  props
+) => {
+  const { level, priority, analysis } = props;
+  return (
+    <section css={styles}>
+      <h3>Skill Mastery</h3>
+      <dl className="mastery-recommendation">
+        <div className="recommended-rank">
+          <dt>Recommended Rank</dt>
+          <dd>{level === "no" ? "None" : `Mastery ${level}`}</dd>
+        </div>
+        <div className="priority">
+          <dt>Priority</dt>
+          <dd>{priority ?? "--"}</dd>
+        </div>
+      </dl>
+      <p dangerouslySetInnerHTML={{ __html: analysis }} />
+    </section>
+  );
+};
+export default MasteryRecommendation;
+
+const styles = (theme: Theme) => css`
+  h3 {
+    font-size: ${theme.typography.generalHeading.fontSize};
+    line-height: ${theme.typography.generalHeading.lineHeight};
+    font-weight: ${theme.typography.generalHeadingBold.fontWeight};
+  }
+
+  .mastery-recommendation {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    grid-gap: ${theme.spacing(0.25)};
+
+    .recommended-rank,
+    .priority {
+      flex-direction: row;
+      align-items: center;
+
+      dd {
+        margin: 0;
+        font-size: ${theme.typography.body.fontSize};
+        line-height: ${theme.typography.body.lineHeight};
+        font-weight: ${theme.typography.bodyBold.fontWeight};
+      }
+    }
+  }
+`;

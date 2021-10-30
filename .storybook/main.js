5da8e70e366eae3d1180e2bd405541ff389f203e
Ian Kim <me@ian.kim>
2021-10-30 16:30:10 -0400

    Use webpack5 to build Storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,9 @@
 module.exports = {
   stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+  core: {
+    builder: "webpack5",
+  },
   babel: async (options) => {
     options.overrides.push({
       presets: [

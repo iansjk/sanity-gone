a1b208995676ee925a96e99cbd46e2f929799cd7
Ian Kim <iansjk@gmail.com>
2021-11-02 10:33:35 -0400

    Add some more error checking/reporting

--- plugins/gatsby-plugin-aceship/gatsby-node.js
+++ plugins/gatsby-plugin-aceship/gatsby-node.js
@@ -4,11 +4,17 @@ const opNameToId = Object.fromEntries(operatorsJson.map((op) => [op.name, op.id]
 const opIdToPath = {};
 
 exports.onCreatePage = ({ page, reporter, actions }) => {
-  if (page.context?.operator__name) {
-    opIdToPath[opNameToId[page.context.operator__name]] = page.path;
+  const opName = page.context?.operator__name;
+  if (opName) {
+    const opId = opNameToId[opName];
+    if (!opId) {
+      reporter.panic(`Couldn't find id for operator name "${opName}"`);
+    } else {
+      opIdToPath[opId] = page.path;
+    }
   }
 }
 
 exports.onPostBootstrap = () => {
-  console.table(opIdToPath);
+  reporter.verbose(`Generated Aceship opId to path json: ${JSON.stringify(opIdToPath, null, 2)}`);
 }

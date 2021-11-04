d29d1e43b01d5d5c39742c05634207adbe4c8c0f
Ian Kim <iansjk@gmail.com>
2021-11-04 01:10:09 -0400

    Replace class/subclass names w/ images

--- src/pages/classes.tsx
+++ src/pages/classes.tsx
@@ -1,15 +1,21 @@
 import { graphql } from "gatsby";
+import slugify from "@sindresorhus/slugify";
+
+import Layout from "../Layout";
+import { operatorClassIcon, operatorSubclassIcon } from "../utils/images";
+import { css, Theme } from "@emotion/react";
 
 interface Props {
   data: {
-    allContentfulOperatorSubclass: {
-      nodes: {
-        subclass: string;
-      }[];
-    };
     allContentfulOperatorClass: {
       nodes: {
         className: string;
+      }[];
+    };
+    allContentfulOperatorSubclass: {
+      nodes: {
+        subclass: string;
+        subProfessionId: string;
       }[];
     };
   };
@@ -20,35 +26,64 @@ const Classes: React.VFC<Props> = ({ data }) => {
   const { nodes: operatorSubclasses } = data.allContentfulOperatorSubclass;
 
   return (
-    <main>
-      <h2>Classes</h2>
-      <ul>
-        {operatorClasses.map(({ className }) => (
-          <li key={className}>{className}</li>
-        ))}
-      </ul>
-      <br />
-      <h2>Subclasses</h2>
-      <ul>
-        {operatorSubclasses.map(({ subclass }) => (
-          <li key={subclass}>{subclass}</li>
-        ))}
-      </ul>
-    </main>
+    <Layout pageTitle="Classes and Subclasses">
+      <main css={styles}>
+        <h2>Classes</h2>
+        <ul>
+          {operatorClasses.map(({ className }) => (
+            <li key={className}>
+              <img
+                width={64}
+                height={64}
+                src={operatorClassIcon(slugify(className))}
+              />
+            </li>
+          ))}
+        </ul>
+        <br />
+        <h2>Subclasses</h2>
+        <ul>
+          {operatorSubclasses.map(({ subclass, subProfessionId }) => (
+            <li key={subclass}>
+              <img
+                width={64}
+                height={64}
+                src={operatorSubclassIcon(subProfessionId)}
+              />
+            </li>
+          ))}
+        </ul>
+      </main>
+    </Layout>
   );
 };
 export default Classes;
 
+const styles = (theme: Theme) => css`
+  ul {
+    display: flex;
+    margin: 0;
+    padding: 0;
+    list-style: none;
+    flex-wrap: wrap;
+  }
+
+  img {
+    object-fit: contain;
+  }
+`;
+
 export const query = graphql`
   query {
-    allContentfulOperatorSubclass {
-      nodes {
-        subclass
-      }
-    }
     allContentfulOperatorClass {
       nodes {
         className
+      }
+    }
+    allContentfulOperatorSubclass {
+      nodes {
+        subclass
+        subProfessionId
       }
     }
   }

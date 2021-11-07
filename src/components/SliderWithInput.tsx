63212b007ddde41367e22920f9f2ccbdc2654c7c
Stinggyray <stinggyray@gmail.com>
2021-11-07 02:15:13 -0600

    fix slider styling

--- src/components/SliderWithInput.tsx
+++ src/components/SliderWithInput.tsx
@@ -70,7 +70,7 @@ const styles = (theme: Theme) => css`
     margin: auto 0;
     padding: ${theme.spacing(0.5, 2.25)};
     border-radius: ${theme.spacing(0.5)};
-    border: ${theme.spacing(0.25)} solid ${theme.palette.midtoneBrighterer.main};
+    border: 1px solid ${theme.palette.midtoneBrighterer.main};
 
     ${theme.breakpoints.down("mobile")} {
       flex-grow: 1;
@@ -125,7 +125,7 @@ const styles = (theme: Theme) => css`
         }
       }
 
-      .MuiSlider-thumb:hover {
+      &:hover .MuiSlider-thumb {
         background: url("data:image/svg+xml,%3Csvg width='11' height='11' viewBox='0 0 11 11' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath fill-rule='evenodd' clip-rule='evenodd' d='M0.5 0.5C0.776143 0.5 1 0.723858 1 1L1 10C1 10.2761 0.776142 10.5 0.5 10.5C0.223858 10.5 -1.20705e-08 10.2761 0 10L3.93396e-07 1C4.05467e-07 0.723858 0.223858 0.5 0.5 0.5Z' fill='%2387879B'/%3E%3Cpath fill-rule='evenodd' clip-rule='evenodd' d='M5.5 0.5C5.77614 0.5 6 0.723858 6 1L6 10C6 10.2761 5.77614 10.5 5.5 10.5C5.22386 10.5 5 10.2761 5 10L5 1C5 0.723858 5.22386 0.5 5.5 0.5Z' fill='%2387879B'/%3E%3Cpath fill-rule='evenodd' clip-rule='evenodd' d='M10.5 0.5C10.7761 0.5 11 0.723858 11 1V10C11 10.2761 10.7761 10.5 10.5 10.5C10.2239 10.5 10 10.2761 10 10V1C10 0.723858 10.2239 0.5 10.5 0.5Z' fill='%2387879B'/%3E%3C/svg%3E%0A")
           no-repeat center ${theme.palette.white.main};
       }

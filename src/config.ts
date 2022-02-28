3b4089718de0fc74857ff1aa7f46e2fa169225bc
Ian Kim <iansjk@gmail.com>
2022-02-27 22:51:40 -0500

    Add theme & config

---
+++ src/config.ts
@@ -0,0 +1,8 @@
+const config = {
+  siteUrl: "https://sanitygone.help",
+  siteName: "Sanity;Gone",
+  image: "/sg-logo.png",
+  description:
+    "Sanity;Gone is a community resource for Arknights players, providing quick guides, reviews, and detailed information about the game.",
+};
+export default config;

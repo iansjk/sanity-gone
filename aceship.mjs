c537bda60fdd48468a913fa0d845143518ce3152
Ian Kim <iansjk@gmail.com>
2022-03-04 22:47:37 -0500

    Add script to generate aceship.json

---
+++ aceship.mjs
@@ -0,0 +1,41 @@
+// Generates aceship.json, a mapping of charIds -> relative page paths
+import path from "path";
+import fs from "fs";
+import { fileURLToPath } from "url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+const prerenderManifestPath = path.join(
+  __dirname,
+  ".next",
+  "prerender-manifest.json"
+);
+const outFilePath = path.join(__dirname, "public", "aceship.json");
+
+void (async () => {
+  const prerenderManifest = (
+    await import(prerenderManifestPath, { assert: { type: "json" } })
+  ).default;
+  const mappings = await Promise.all(
+    Object.keys(prerenderManifest.routes)
+      .filter((route) => route.startsWith("/operators/"))
+      .map(async (route) => {
+        const [_root, __operators, slug] = route.split("/");
+        const pageJsonPath = path.join(
+          __dirname,
+          ".next",
+          "server",
+          "pages",
+          "operators",
+          `${slug}.json`
+        );
+        const pageJson = (
+          await import(pageJsonPath, { assert: { type: "json" } })
+        ).default;
+        const { charId } = pageJson.pageProps;
+        return [charId, route];
+      })
+  );
+  const aceshipJson = Object.fromEntries(mappings);
+  fs.writeFileSync(outFilePath, JSON.stringify(aceshipJson));
+})();

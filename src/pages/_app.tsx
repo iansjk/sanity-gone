80bb09751cdd6035920770f2542be86cffc29056
Ian Kim <iansjk@gmail.com>
2022-03-09 22:59:13 -0500

    Load Google Analytics in prod

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,9 +9,13 @@ import { MediaContextProvider, mediaStyle } from "../Media";
 
 import type { AppProps } from "next/app";
 import Head from "next/head";
+import Script from "next/script";
+import { GA_TRACKING_ID, pageview } from "../utils/gtag";
 
 // Client-side cache, shared for the whole session of the user in the browser.
 const clientSideEmotionCache = createEmotionCache();
+
+const isProductionEnvironment = process.env.NODE_ENV === "production";
 
 interface MyAppProps extends AppProps {
   emotionCache?: EmotionCache;
@@ -32,6 +36,31 @@ export default function MyApp(props: MyAppProps) {
                 content="initial-scale=1, width=device-width"
               />
             </Head>
+
+            {/* Global Site Tag (gtag.js) - Google Analytics */}
+            {/* from https://github.com/vercel/next.js/blob/canary/examples/with-google-analytics/pages/_app.js */}
+            {isProductionEnvironment && (
+              <>
+                <Script
+                  strategy="afterInteractive"
+                  src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+                />
+                <Script
+                  id="gtag-init"
+                  strategy="afterInteractive"
+                  dangerouslySetInnerHTML={{
+                    __html: `
+                  window.dataLayer = window.dataLayer || [];
+                  function gtag(){dataLayer.push(arguments);}
+                  gtag('js', new Date());
+                  gtag('config', '${GA_TRACKING_ID}', {
+                    page_path: window.location.pathname,
+                  });
+                `,
+                  }}
+                />
+              </>
+            )}
 
             <Component {...pageProps} />
           </EmotionThemeProvider>

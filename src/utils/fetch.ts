0062f4968b4bd28f0443269ea2f6cf3250e92ff9
Ian Kim <iansjk@gmail.com>
2022-03-10 00:28:22 -0500

    Add nullish checks for Contentful envvars

--- src/utils/fetch.ts
+++ src/utils/fetch.ts
@@ -1,11 +1,20 @@
 import axios from "axios";
 import "dotenv/config";
 
+if (process.env.CONTENTFUL_ACCESS_TOKEN == null) {
+  throw new Error("CONTENTFUL_ACCESS_TOKEN is not defined");
+}
+if (process.env.CONTENTFUL_SPACE_ID == null) {
+  throw new Error("CONTENTFUL_SPACE_ID is not defined");
+}
+const CONTENTFUL_ACCESS_TOKEN = process.env.CONTENTFUL_ACCESS_TOKEN;
+const CONTENTFUL_SPACE_ID = process.env.CONTENTFUL_SPACE_ID;
+
 const contentfulGraphQlApiUrl = `https://graphql.contentful.com/content/v1/spaces`;
 const instance = axios.create({
   baseURL: contentfulGraphQlApiUrl,
   headers: {
-    Authorization: `Bearer ${process.env.CONTENTFUL_ACCESS_TOKEN!}`,
+    Authorization: `Bearer ${CONTENTFUL_ACCESS_TOKEN}`,
     "Content-Type": "application/json; charset=utf-8",
   },
   timeout: 1000,
@@ -17,13 +26,10 @@ export async function fetchContentfulGraphQl<T = any>(
 ): Promise<T> {
   let response = null;
   try {
-    response = await instance.post<{ data: T }>(
-      "/" + process.env.CONTENTFUL_SPACE_ID!,
-      {
-        query,
-        variables,
-      }
-    );
+    response = await instance.post<{ data: T }>("/" + CONTENTFUL_SPACE_ID, {
+      query,
+      variables,
+    });
     if (response.status !== 200) {
       throw new Error(response.statusText);
     }

248cc5a7802f589ff9b2d89bf8e01147e18a01cd
Ian Kim <iansjk@gmail.com>
2022-06-29 22:04:56 -0400

    Add support for incantationmedic branch

--- scripts/copy-gamedata.ts
+++ scripts/copy-gamedata.ts
@@ -78,6 +78,7 @@ const CN_BRANCH_TLS: Record<string, string> = {
   fortress: "Fortress",
   wandermedic: "Wandering",
   craftsman: "Artificer",
+  incantationmedic: "Incantation",
 };
 // Separate EN overrides.
 // Kept separate from the above overrides for the sake of clarity.
@@ -118,6 +119,8 @@ const TRAIT_OVERRIDES: Record<string, string> = {
     "Prioritize <@ba.kw>Long range splash attack</> (splash radius of <@ba.kw>1.0</> tiles) when not blocking",
   funnel:
     "Controls a <@ba.kw>Drone</> that deals <@ba.kw>Arts damage</>; When the Drone continuously attacks the same enemy, its damage will increase (from 10% up to 110% of the operator's ATK, linearly)",
+  incantationmedic:
+    "Attacks deal <@ba.kw>Arts damage</>. When attacking an enemy, heal a friendly operator within attack range for <@ba.kw>50%</> of the damage dealt.",
 };
 
 // Translations for modules that are not yet in EN.
@@ -395,10 +398,8 @@ void (async () => {
           if (!description) {
             continue;
           }
-          moduleEffect += descriptionToHtml(
-            description,
-            curCandidates[j].blackboard
-          ) + "\n";
+          moduleEffect +=
+            descriptionToHtml(description, curCandidates[j].blackboard) + "\n";
         }
       }
     }

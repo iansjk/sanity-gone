c7b075749259e61c7f1447f3b332605e7a829ba8
Ian Kim <me@ian.kim>
2022-08-16 19:00:12 -0400

    Manually add <p> to Synergy story analyses

--- src/components/Synergy.stories.tsx
+++ src/components/Synergy.stories.tsx
@@ -17,7 +17,7 @@ Angelina.args = {
   profession: "SUPPORT",
   subProfessionId: "slower",
   analysis: `
-    Her benefits to Mudrock are entirely passive, and Angelina is great for general use anyway so it makes it super easy to have them together. Angelina passively provides global HP regen and an ASPD buff, since the regen is obviously not a direct heal, it works perfectly fine on Mudrock. The ASPD is also always good since Mudrock is also a damage dealer. The passive ASPD also helps if you’re bringing Blemishine too, since faster attacks means faster charging for Mudrock S2 while Blemi is deployed. Team building matters, folks! As a bonus, Angelina’s crowd control capabilities can help with stalling during Mudrock’s S3 startup, since her fast skill rotations means Angelina can always be ready to be used together with Mudrock’s S3.
+    <p>Her benefits to Mudrock are entirely passive, and Angelina is great for general use anyway so it makes it super easy to have them together. Angelina passively provides global HP regen and an ASPD buff, since the regen is obviously not a direct heal, it works perfectly fine on Mudrock. The ASPD is also always good since Mudrock is also a damage dealer. The passive ASPD also helps if you’re bringing Blemishine too, since faster attacks means faster charging for Mudrock S2 while Blemi is deployed. Team building matters, folks! As a bonus, Angelina’s crowd control capabilities can help with stalling during Mudrock’s S3 startup, since her fast skill rotations means Angelina can always be ready to be used together with Mudrock’s S3.</p>
   `,
 };
 
@@ -27,5 +27,5 @@ LowAtkOperators.args = {
   isGroup: true,
   iconUrl:
     "https://res.cloudinary.com/samidare/image/upload/v1628565419/arknights/subclasses/fastshot.png",
-  analysis: `The toughest thing for a lot of these operators, such as AA snipers, is how much even mid-levels of DEF can hurt their DPS. Skalter is able to provide them with a strong flat ATK buff either at short, strong bursts, or with her permanent duration buff. She massively improves their DPS and makes them much better to use in general with no downside.`,
+  analysis: `<p>The toughest thing for a lot of these operators, such as AA snipers, is how much even mid-levels of DEF can hurt their DPS. Skalter is able to provide them with a strong flat ATK buff either at short, strong bursts, or with her permanent duration buff. She massively improves their DPS and makes them much better to use in general with no downside.</p>`,
 };

533a89fd51158f494ad8024f732748c6518c4f26
Ian Kim <iansjk@gmail.com>
2022-09-13 23:21:11 -0400

    Add character range for Rosa E2

--- src/components/CharacterRange.stories.tsx
+++ src/components/CharacterRange.stories.tsx
@@ -182,3 +182,65 @@ EyjaS3.args = {
     ],
   },
 };
+
+export const RosaE2 = Template.bind({});
+RosaE2.args = {
+  rangeObject: {
+    id: "4-3",
+    direction: 1,
+    grids: [
+      {
+        row: 2,
+        col: 2,
+      },
+      {
+        row: 2,
+        col: 3,
+      },
+      {
+        row: 1,
+        col: 2,
+      },
+      {
+        row: 1,
+        col: 3,
+      },
+      {
+        row: 1,
+        col: 4,
+      },
+      {
+        row: 0,
+        col: 2,
+      },
+      {
+        row: 0,
+        col: 3,
+      },
+      {
+        row: 0,
+        col: 4,
+      },
+      {
+        row: -1,
+        col: 2,
+      },
+      {
+        row: -1,
+        col: 3,
+      },
+      {
+        row: -1,
+        col: 4,
+      },
+      {
+        row: -2,
+        col: 2,
+      },
+      {
+        row: -2,
+        col: 3,
+      },
+    ],
+  },
+};

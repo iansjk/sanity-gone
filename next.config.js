0ba354015ad6ce14dfdb481bd18db35efeab8cab
Ian Kim <iansjk@gmail.com>
2022-09-24 16:43:43 -0400

    Use preact

--- next.config.js
+++ next.config.js
@@ -18,4 +18,7 @@ const nextConfig = {
 const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: process.env.ANALYZE === "true",
 });
-module.exports = withBundleAnalyzer(nextConfig);
+
+const withPreact = require("next-plugin-preact");
+
+module.exports = withPreact(withBundleAnalyzer(nextConfig));

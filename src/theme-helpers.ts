0571b7c2d051f2dab44ae11f70d84637261b5134
Ian Kim <me@ian.kim>
2022-09-29 01:39:54 -0400

    Create global theme and helpers for vanilla-extract

---
+++ src/theme-helpers.ts
@@ -0,0 +1,16 @@
+const spacingUnitInPixels = 8;
+
+export const spacing = (...argsInput: number[]): string => {
+  const args = argsInput.length === 0 ? [1] : argsInput;
+  return args.map((arg) => `${arg * spacingUnitInPixels}px`).join(" ");
+};
+
+const breakpointValues = {
+  mobile: "1000px",
+  maxWidth: `${1270 + spacingUnitInPixels * 3 * 2}px`,
+};
+
+export const breakpoints = {
+  down: (bp: keyof typeof breakpointValues) => `(max-width: ${bp})`,
+  up: (bp: keyof typeof breakpointValues) => `(min-width: ${bp})`,
+};
---
+++ src/theme.css.ts
@@ -0,0 +1,121 @@
+import { createGlobalTheme } from "@vanilla-extract/css";
+
+export const vars = createGlobalTheme(":root", {
+  colors: {
+    neutrals: {
+      white: "#e8e8f2",
+      gray: "#87879b",
+      black: "#101014",
+      blackest: "#050507",
+      darktone: "#14141b",
+      midtoneDarker: "#191920",
+      midtone: "#24242e",
+      midtoneBrighter: "#363643",
+      midtoneBrighterer: "#484858",
+      midtoneExtra: "#1f1f27",
+    },
+    accents: {
+      red: "#fb4040",
+      orange: "#f98d3f",
+      yellow: "#ffcf53",
+      lime: "#a7e855",
+      blue: "#49b3ff",
+      softBlue: "#7f7dea",
+      purple: "7F7DEA",
+      magenta: "#e85593",
+    },
+  },
+  typography: {
+    fontFamily: "Source Sans Pro, sans-serif",
+    pageHeading: {
+      fontSize: "48px",
+      fontWeight: "600",
+      lineHeight: "1",
+    },
+    operatorPageHeading: {
+      fontSize: "72px",
+      fontWeight: "600",
+      lineHeight: "1",
+    },
+    operatorNameHeading: {
+      fontSize: "36px",
+      fontWeight: "600",
+      lineHeight: "1.25",
+    },
+    cardHeading: {
+      fontSize: "24px",
+      fontWeight: "700",
+      lineHeight: "1.25",
+      textTransform: "uppercase",
+    },
+    generalHeading: {
+      fontSize: "24px",
+      lineHeight: "1.25",
+    },
+    generalHeadingBold: {
+      fontSize: "24px",
+      fontWeight: "600",
+      lineHeight: "1.25",
+    },
+    skillTalentHeading: {
+      fontSize: "18px",
+      fontWeight: "600",
+      lineHeight: "1.25",
+    },
+    body1: {
+      fontSize: "18px",
+      lineHeight: "1.5",
+      fontWeight: "400",
+    },
+    body1Bold: {
+      fontWeight: "600",
+    },
+    body1Bolder: {
+      fontWeight: "700",
+    },
+    body2: {
+      fontSize: "16px",
+      lineHeight: "1.5",
+    },
+    body2Bold: {
+      fontWeight: "600",
+    },
+    body3: {
+      fontSize: "14px",
+      lineHeight: "18px",
+    },
+    smallPortraitRarity: {
+      fontSize: "18px",
+      lineHeight: "1.25",
+      fontWeight: "700",
+    },
+    label1: {
+      fontSize: "12px",
+      fontWeight: "600",
+      lineHeight: "1.25",
+    },
+    label2: {
+      fontSize: "14px",
+      fontWeight: "600",
+      lineHeight: "1.25",
+    },
+    navigationLink: {
+      fontSize: "18px",
+      lineHeight: "1.25",
+    },
+    navigationLinkBold: {
+      fontWeight: "600",
+    },
+    operatorBrowserNameHeading: {
+      fontSize: "16px",
+      fontWeight: "600",
+      lineHeight: "1.25",
+    },
+    operatorCardAlterName: {
+      fontSize: "14px",
+      fontWeight: "400",
+      lineHeight: "1.25",
+      textTransform: "uppercase",
+    },
+  },
+});

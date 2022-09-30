bec332d09a300ea3335996b615722c180775a4db
Ian Kim <iansjk@gmail.com>
2022-09-30 04:31:34 -0400

    Forward elite icons' className prop to the inner path element

--- src/components/icons/operatorStats/EliteOneIcon.tsx
+++ src/components/icons/operatorStats/EliteOneIcon.tsx
@@ -1,4 +1,7 @@
-const EliteOneIcon: React.VFC<React.HTMLAttributes<SVGElement>> = (props) => {
+const EliteOneIcon: React.VFC<React.HTMLAttributes<SVGElement>> = ({
+  className,
+  ...rest
+}) => {
   return (
     <svg
       width="28"
@@ -6,11 +9,12 @@ const EliteOneIcon: React.VFC<React.HTMLAttributes<SVGElement>> = (props) => {
       viewBox="0 0 28 18"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
-      {...props}
+      {...rest}
     >
       <path
         d="M14.0001 13.106L6.72006 17.96L8.80006 12.84L11.2001 11.24L2.08006 5.16004L6.10352e-05 0.0400391L14.0001 9.37404L28.0001 0.0400391L25.9201 5.16004L16.8001 11.24L19.2001 12.84L21.2801 17.96L14.0001 13.106Z"
         fill="#484858"
+        className={className}
       />
     </svg>
   );

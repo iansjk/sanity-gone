369e49a55aa6c831dc4ad1b1c4d784b86c10be7e
Ian Kim <iansjk@gmail.com>
2022-09-30 04:48:58 -0400

    Revert "Forward elite icons' className prop to the inner path element"
    This reverts commit bfd4a79f23226e5b5c0deeb4fdb18113d0fb3dd1.

--- src/components/icons/operatorStats/EliteTwoIcon.tsx
+++ src/components/icons/operatorStats/EliteTwoIcon.tsx
@@ -1,7 +1,4 @@
-const EliteTwoIcon: React.VFC<React.HTMLAttributes<SVGElement>> = ({
-  className,
-  ...rest
-}) => {
+const EliteTwoIcon: React.VFC<React.HTMLAttributes<SVGElement>> = (props) => {
   return (
     <svg
       width="28"
@@ -9,14 +6,13 @@ const EliteTwoIcon: React.VFC<React.HTMLAttributes<SVGElement>> = ({
       viewBox="0 0 28 26"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
-      {...rest}
+      {...props}
     >
       <path
         fillRule="evenodd"
         clipRule="evenodd"
         d="M6.72 25.3203L14 20.4663L21.28 25.3203L19.2 20.2003L16.8 18.6003L19.0406 17.1066L21.28 18.5997L20.3254 16.25L25.92 12.5203L28 7.40028L19.0408 13.3735L16.8 11.8797L25.92 5.79969L28 0.679688L14 10.0137L0 0.679688L2.08 5.79969L11.2 11.8797L8.95923 13.3735L0 7.40028L2.08 12.5203L7.67456 16.25L6.72 18.5997L8.9594 17.1066L11.2 18.6003L8.8 20.2003L6.72 25.3203ZM14 13.7457L11.7588 15.24L14 16.7343L16.2412 15.24L14 13.7457Z"
         fill="#E8E8F2"
-        className={className}
       />
     </svg>
   );
--- src/components/icons/operatorStats/EliteZeroIcon.tsx
+++ src/components/icons/operatorStats/EliteZeroIcon.tsx
@@ -1,7 +1,4 @@
-const EliteZeroIcon: React.VFC<React.HTMLAttributes<SVGElement>> = ({
-  className,
-  ...rest
-}) => {
+const EliteZeroIcon: React.VFC<React.HTMLAttributes<SVGElement>> = (props) => {
   return (
     <svg
       width="28"
@@ -9,12 +6,11 @@ const EliteZeroIcon: React.VFC<React.HTMLAttributes<SVGElement>> = ({
       viewBox="0 0 34 24"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
-      {...rest}
+      {...props}
     >
       <path
         d="M13.2986 14.2391L11.5226 15.423L11.3947 15.5084L11.3368 15.6509L9.25677 20.7709L8.64499 22.2768L9.99738 21.3751L17 16.706L24.0026 21.3751L25.355 22.2768L24.7432 20.7709L22.6632 15.6509L22.6053 15.5084L22.4774 15.423L20.7014 14.2391L29.1974 8.57509L29.3253 8.48976L29.3832 8.34725L31.4632 3.22725L32.075 1.72142L30.7226 2.62305L17 11.7721L3.27736 2.62305L1.92502 1.72142L2.53677 3.22725L4.61677 8.34725L4.67466 8.48976L4.80265 8.57509L13.2986 14.2391Z"
         stroke="#484858"
-        className={className}
       />
     </svg>
   );

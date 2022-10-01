f89767451798b1bd0c439249c1515fa2108451d9
Blede <maxtergameplay@gmail.com>
2022-10-01 11:51:05 -0400

    migrated mastery recommendation

--- src/components/MasteryRecommendation/MasteryRecommendation.tsx
+++ src/components/MasteryRecommendation/MasteryRecommendation.tsx
@@ -1,5 +1,4 @@
-import { css } from "@emotion/react";
-import { Theme } from "@mui/material";
+import * as classes from "./styles.css";
 
 export interface MasteryRecommendationProps {
   level: "no" | "1" | "2" | "3";
@@ -10,16 +9,18 @@ export interface MasteryRecommendationProps {
 const MasteryRecommendation: React.FC<MasteryRecommendationProps> = (props) => {
   const { level, priority, children } = props;
   return (
-    <section css={styles}>
+    <section>
       <h3>Skill Mastery</h3>
-      <dl className="mastery-recommendation">
-        <div className="recommended-rank">
-          <dt>Recommended Rank</dt>
-          <dd>{level === "no" ? "None" : `Mastery ${level}`}</dd>
+      <dl className={classes.masteryRecommendation}>
+        <div className={classes.recommendedRank}>
+          <dt className={classes.descriptionTitle}>Recommended Rank</dt>
+          <dd className={classes.description}>
+            {level === "no" ? "None" : `Mastery ${level}`}
+          </dd>
         </div>
-        <div className="priority">
-          <dt>Priority</dt>
-          <dd>{priority ?? "--"}</dd>
+        <div className={classes.priority}>
+          <dt className={classes.descriptionTitle}>Priority</dt>
+          <dd className={classes.description}>{priority ?? "--"}</dd>
         </div>
       </dl>
       {children}
@@ -27,51 +28,3 @@ const MasteryRecommendation: React.FC<MasteryRecommendationProps> = (props) => {
   );
 };
 export default MasteryRecommendation;
-
-const styles = (theme: Theme) => css`
-  h3 {
-    font-size: ${theme.typography.generalHeading.fontSize}px;
-    line-height: ${theme.typography.generalHeading.lineHeight};
-    font-weight: ${theme.typography.generalHeadingBold.fontWeight};
-  }
-
-  .mastery-recommendation {
-    display: grid;
-    grid-template-columns: repeat(2, 1fr);
-    grid-gap: ${theme.spacing(0.25)};
-
-    ${theme.breakpoints.down("mobile")} {
-      grid-template-columns: unset;
-    }
-
-    .recommended-rank {
-      border-radius: ${theme.spacing(0.5, 0, 0, 0.5)};
-      ${theme.breakpoints.down("mobile")} {
-        border-radius: ${theme.spacing(0.5, 0.5, 0, 0)};
-      }
-    }
-    .priority {
-      border-radius: ${theme.spacing(0, 0.5, 0.5, 0)};
-      ${theme.breakpoints.down("mobile")} {
-        border-radius: ${theme.spacing(0, 0, 0.5, 0.5)};
-      }
-    }
-    .recommended-rank,
-    .priority {
-      flex-direction: row;
-      align-items: center;
-
-      dt {
-        font-size: ${theme.typography.body1.fontSize}px;
-        line-height: ${theme.typography.body1.lineHeight};
-      }
-
-      dd {
-        margin: 0;
-        font-size: ${theme.typography.body1.fontSize}px;
-        line-height: ${theme.typography.body1.lineHeight};
-        font-weight: ${theme.typography.body1Bold.fontWeight};
-      }
-    }
-  }
-`;

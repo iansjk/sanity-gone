0ad4e4e3e27f3ee202829a8ff16ac0cb7fd7438b
Ian Kim <me@ian.kim>
2022-10-01 02:31:52 -0400

    Port Card styles to vanilla-extract

---
+++ src/components/Card/Card.tsx
@@ -0,0 +1,25 @@
+import cx from "clsx";
+import * as classes from "./styles.css";
+
+export type CardProps = React.HTMLAttributes<HTMLDivElement> & {
+  header: string;
+};
+
+const Card: React.FC<CardProps> = (props) => {
+  const { header, children, dangerouslySetInnerHTML, className, ...rest } =
+    props;
+  return (
+    <section className={cx(classes.root, className)} {...rest}>
+      <div className={classes.headingBlock}>
+        <h2 className={classes.heading}>{header}</h2>
+      </div>
+      <div
+        className={classes.cardContent}
+        dangerouslySetInnerHTML={dangerouslySetInnerHTML}
+      >
+        {children}
+      </div>
+    </section>
+  );
+};
+export default Card;
---
+++ src/components/Card/styles.css.ts
@@ -0,0 +1,52 @@
+import { globalStyle, style } from "@vanilla-extract/css";
+import { transparentize } from "polished";
+import { breakpoints, spacing } from "../../theme-helpers";
+import { rawColors, vars } from "../../theme.css";
+
+export const root = style({
+  borderRadius: spacing(1),
+  padding: 0,
+  boxShadow: `${spacing(0.25)} ${spacing(0.5)} ${spacing(
+    1
+  )} rgba(0, 0, 0, 0.05)`,
+});
+
+export const headingBlock = style({
+  padding: spacing(2, 0, 2, 4),
+  borderTopRightRadius: spacing(1),
+  "@media": {
+    [breakpoints.down("mobile")]: {
+      display: "none",
+    },
+  },
+});
+
+export const heading = style({
+  margin: 0,
+  textTransform: vars.typography.cardHeading.textTransform,
+  fontSize: vars.typography.cardHeading.fontSize,
+  fontWeight: vars.typography.cardHeading.fontWeight,
+  lineHeight: vars.typography.cardHeading.lineHeight,
+  color: vars.colors.neutrals.white,
+});
+
+export const cardContent = style({
+  padding: spacing(3, 4, 4),
+  background: transparentize(0.34, rawColors.neutrals.midtone),
+  borderBottomRightRadius: spacing(1),
+  "@media": {
+    [breakpoints.down("mobile")]: {
+      padding: spacing(2),
+      borderBottomRightRadius: "unset",
+    },
+  },
+});
+
+globalStyle(`${cardContent} > p`, {
+  margin: spacing(3, 0, 0),
+  "@media": {
+    [breakpoints.down("mobile")]: {
+      margin: spacing(2, 0, 0),
+    },
+  },
+});

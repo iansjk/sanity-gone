c050283d2ecf0bafa163e30d7590b7d620d3323b
Ian Kim <me@ian.kim>
2022-10-06 14:54:47 -0400

    Shorten tooltip animation slightly
    [300, 250] -> [250, 250]
    (show, hide)

--- src/components/Tooltip/Tooltip.tsx
+++ src/components/Tooltip/Tooltip.tsx
@@ -10,7 +10,12 @@ import type { TippyProps } from "@tippyjs/react";
 const Tooltip: React.VFC<TippyProps> = ({ children, theme, ...rest }) => {
   return (
     // offset: 8px is the height of the tooltip arrow, then spacing(0.5) is 4px
-    <Tippy theme={theme ?? themeName} offset={[0, 8 + 4]} {...rest}>
+    <Tippy
+      theme={theme ?? themeName}
+      offset={[0, 8 + 4]}
+      duration={[250, 250]}
+      {...rest}
+    >
       {children}
     </Tippy>
   );

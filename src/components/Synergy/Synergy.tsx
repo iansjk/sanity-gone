2b8b0c8e66e55a05f08b1be5d5928f106dc82825
Blede <maxtergameplay@gmail.com>
2022-10-09 19:24:25 -0400

    removed comments

--- src/components/Synergy/Synergy.tsx
+++ src/components/Synergy/Synergy.tsx
@@ -59,11 +59,11 @@ const Synergy: React.VFC<
           <h3 className={classes.operatorName}>{name}</h3>
           {!isGroup && (
             <div className={classes.synergyOperatorInfo}>
-              {/* eslint-disable-next-line @typescript-eslint/no-non-null-assertion */}
               <span
                 className={
-                  classes.synergyQuality[
-                    `rarity-${rarity!}-stars` as keyof typeof classes.synergyQuality
+                  classes.rarity[
+                    /* eslint-disable-next-line @typescript-eslint/no-non-null-assertion */
+                    `rarity-${rarity!}-stars` as keyof typeof classes.rarity
                   ]
                 }
               >
@@ -92,76 +92,3 @@ const Synergy: React.VFC<
   );
 };
 export default Synergy;
-
-const styles = (theme: Theme) => css`
-  margin-top: ${theme.spacing(3)};
-  flex-direction: column;
-
-  :not([hidden]) {
-    display: flex;
-  }
-
-  .synergy-header {
-    display: flex;
-    align-items: center;
-
-    .portrait {
-      position: relative;
-
-      .group-synergy-icon {
-        position: absolute;
-        right: -3px;
-        bottom: -2px;
-      }
-    }
-
-    .name-and-quality {
-      margin-left: ${theme.spacing(2)};
-
-      .operator-name {
-        margin: 0;
-        font-size: ${theme.typography.generalHeadingBold.fontSize}px;
-        font-weight: ${theme.typography.generalHeadingBold.fontWeight};
-        line-height: ${theme.typography.generalHeadingBold.lineHeight};
-      }
-
-      .synergy-quality {
-        font-size: ${theme.typography.label2.fontSize}px;
-        text-transform: uppercase;
-        line-height: ${theme.typography.label2.lineHeight};
-        color: ${theme.palette.gray.main};
-
-        &.quality--1 {
-          color: ${theme.palette.red.main};
-        }
-
-        &.quality-1 {
-          color: ${theme.palette.blue.main};
-        }
-
-        &.quality-2 {
-          color: ${theme.palette.lime.main};
-        }
-      }
-
-      .synergy-operator-info {
-        font-size: ${theme.typography.body3.fontSize}px;
-        line-height: ${theme.typography.body3.lineHeight};
-
-        .operator-class,
-        .class-subclass-separator,
-        .subclass {
-          display: inline-block;
-        }
-
-        .operator-class {
-          margin-left: ${theme.spacing(1)};
-        }
-
-        .class-subclass-separator {
-          margin: ${theme.spacing(0, 1)};
-        }
-      }
-    }
-  }
-`;
--- src/components/Synergy/styles.css.ts
+++ src/components/Synergy/styles.css.ts
@@ -66,75 +66,31 @@ export const classSubclassSeparator = style({
   display: "inline-block",
 });
 
-// const styles = (theme: Theme) => css`
-//   margin-top: ${theme.spacing(3)};
-//   flex-direction: column;
-
-//   :not([hidden]) {
-//     display: flex;
-//   }
-
-//   .synergy-header {
-//     display: flex;
-//     align-items: center;
-
-//     .portrait {
-//       position: relative;
-
-//       .group-synergy-icon {
-//         position: absolute;
-//         right: -3px;
-//         bottom: -2px;
-//       }
-//     }
-
-//     .name-and-quality {
-//       margin-left: ${theme.spacing(2)};
-
-//       .operator-name {
-//         margin: 0;
-//         font-size: ${theme.typography.generalHeadingBold.fontSize}px;
-//         font-weight: ${theme.typography.generalHeadingBold.fontWeight};
-//         line-height: ${theme.typography.generalHeadingBold.lineHeight};
-//       }
-
-//       .synergy-quality {
-//         font-size: ${theme.typography.label2.fontSize}px;
-//         text-transform: uppercase;
-//         line-height: ${theme.typography.label2.lineHeight};
-//         color: ${theme.palette.gray.main};
-
-//         &.quality--1 {
-//           color: ${theme.palette.red.main};
-//         }
-
-//         &.quality-1 {
-//           color: ${theme.palette.blue.main};
-//         }
-
-//         &.quality-2 {
-//           color: ${theme.palette.lime.main};
-//         }
-//       }
-
-//       .synergy-operator-info {
-//         font-size: ${theme.typography.body3.fontSize}px;
-//         line-height: ${theme.typography.body3.lineHeight};
-
-//         .operator-class,
-//         .class-subclass-separator,
-//         .subclass {
-//           display: inline-block;
-//         }
-
-//         .operator-class {
-//           margin-left: ${theme.spacing(1)};
-//         }
-
-//         .class-subclass-separator {
-//           margin: ${theme.spacing(0, 1)};
-//         }
-//       }
-//     }
-//   }
-// `;
+export const rarity = styleVariants({
+  "rarity-6-stars": [
+    {
+      color: vars.colors.accents.orange,
+    },
+  ],
+  "rarity-5-stars": [
+    {
+      color: vars.colors.accents.yellow,
+    },
+  ],
+  "rarity-4-stars": [
+    {
+      color: vars.colors.accents.softBlue,
+    },
+  ],
+  "rarity-3-stars": [
+    {
+      color: vars.colors.accents.blue,
+    },
+  ],
+  "rarity-2-stars": [
+    {
+      color: vars.colors.neutrals.white,
+    },
+  ],
+  "rarity-1-stars": [{ color: vars.colors.neutrals.white }],
+});
